d2214bd62df642a01241262899f38ff1329f405d
ThiagoMaurat <thiagomaurat@hotmail.com>
2023-05-03 08:50:09 -0300

    feat: tests / controllers

---
+++ src/http/controllers/gyms/create.ts
@@ -0,0 +1,32 @@
+import { z } from "zod";
+import { FastifyRequest, FastifyReply } from "fastify";
+import { makeCreateGymInUseCase } from "@/use-cases/factories/make-create-gym-use-case";
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const createGymBodySchema = z.object({
+    title: z.string(),
+    description: z.string().nullable(),
+    phone: z.string().nullable(),
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90;
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180;
+    }),
+  });
+
+  const { description, latitude, phone, title, longitude } =
+    createGymBodySchema.parse(request.body);
+
+  const registerUseCase = makeCreateGymInUseCase();
+
+  await registerUseCase.execute({
+    title,
+    description,
+    latitude,
+    longitude,
+    phone,
+  });
+
+  return reply.status(201).send();
+}
---
+++ src/use-cases/factories/make-create-gym-use-case.ts
@@ -0,0 +1,9 @@
+import { PrismaGymsRepository } from "@/repositories/prisma/prisma-gyms-repository";
+import { CreateGymUseCase } from "../create-gym";
+
+export function makeCreateGymInUseCase() {
+  const prismaGymsRepository = new PrismaGymsRepository();
+  const useCase = new CreateGymUseCase(prismaGymsRepository);
+
+  return useCase;
+}
